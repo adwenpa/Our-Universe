611b75d3c72e6ac52d14ec4e9572bdee954cac2d
adwenpa <joojo.abr@gmail.com>
2025-03-26 01:20:08 -0400

    Polished apod.css and index.html, installed nodemon and notes.js

---
+++ notes.js
@@ -0,0 +1,2 @@
+// NASA APOD APIKEY
+export const apiKey = "B8qwCBXeKqp7aY0wL4cTUwcrSSzhtfdj2lYZEQqy";
--- public/js/apod.js
+++ public/js/apod.js
@@ -1,24 +1,55 @@
-"use strict";
+// import { apiKey } from "../notes.js";
 
-const apiKey = "DEMO_KEY"; // Replace with your NASA API key
-const apodContainer = document.getElementById("apod-container");
+/*
+ * Replace with NASA API key if you intend to download more than thirty
+ * APODs in an hour, or more than 50 APODs in a day.
+ */
+
+const apiKey = "DEMO_KEY";
+
+const heading = document.getElementById("topHeading");
+const apodTitle = document.getElementById("apod-title");
+const apodMedia = document.getElementById("apod-media");
+const apodDescription = document.getElementById("description");
+const apodDate = document.getElementById("date");
+const copyright = document.getElementById("copyright");
+const startDate = document.getElementById("start-date");
 
 async function fetchAPOD() {
-  const response = await fetch(
-    `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`
-  );
-  const data = await response.json();
+  try {
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`)
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        displayAPOD(json);
+      });
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-  const title = data.title;
-  const imageUrl = data.url;
-  const explanation = data.explanation;
-  const date = data.date;
+function displayAPOD(data) {
+  const today = new Date().toISOString().slice(0, 10);
+  startDate.setAttribute("max", `${today}`);
+  const mediaUrl = data.url;
+  const altText = mediaUrl.split("/")[mediaUrl.split("/").length - 1];
 
-  document.getElementById("title").textContent = title;
-  document.getElementById("apod-image").src = imageUrl;
-  document.getElementById("apod-image").alt = title;
-  document.getElementById("description").textContent = explanation;
-  document.getElementById("date").textContent = `Date: ${date}`;
+  heading.textContent = `ASTRONOMY PICTURE OF THE DAY (APOD) ${data.date}`;
+  apodTitle.textContent = data.title;
+  apodDescription.textContent = data.explanation;
+  if (data.media_type === "image") {
+    apodMedia.innerHTML = `<img src=${mediaUrl} 
+  alt=${altText} />`;
+  } else {
+    apodMedia.innerHTML = `<video autoplay muted>
+  <source src="mediaUrl" type="video/mp4">
+Your browser does not support the video tag.
+</video>`;
+  }
+
+  if (data.hasOwnProperty("copyright")) {
+    copyright.innerHTML = `<p>&copy; ${data.copyright}</p>`;
+  }
 }
 
 fetchAPOD();
