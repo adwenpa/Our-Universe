160270d4911b86e1caeeb7f16fed34f6c3457e56
adwenpa <joojo.abr@gmail.com>
2025-03-26 22:43:53 -0400

    Wrote randomDay and dateToISO functions to use in randomBtn.addEventListener.

--- public/js/apod.js
+++ public/js/apod.js
@@ -4,8 +4,9 @@
  * Replace with NASA API key if you intend to download more than thirty
  * APODs in an hour, or more than 50 APODs in a day.
  */
-
+const apiUrl = "https://api.nasa.gov/planetary/apod?api_key=";
 const apiKey = "DEMO_KEY";
+let apiQuery = apiUrl + apiKey;
 
 const heading = document.getElementById("topHeading");
 const apodTitle = document.getElementById("apod-title");
@@ -14,10 +15,11 @@ const apodDescription = document.getElementById("description");
 const apodDate = document.getElementById("date");
 const copyright = document.getElementById("copyright");
 const startDate = document.getElementById("start-date");
+let numApods;
 
 async function fetchAPOD() {
   try {
-    fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`)
+    fetch(apiQuery)
       .then((response) => response.json())
       .then((json) => {
         console.log(json);
@@ -29,7 +31,8 @@ async function fetchAPOD() {
 }
 
 function displayAPOD(data) {
-  const today = new Date().toISOString().slice(0, 10);
+  let today = new Date();
+  today = dateToISO(today);
   startDate.setAttribute("max", `${today}`);
   const mediaUrl = data.url;
   const altText = mediaUrl.split("/")[mediaUrl.split("/").length - 1];
@@ -41,15 +44,99 @@ function displayAPOD(data) {
     apodMedia.innerHTML = `<img src=${mediaUrl} 
   alt=${altText} />`;
   } else {
-    apodMedia.innerHTML = `<video autoplay muted>
-  <source src="mediaUrl" type="video/mp4">
-Your browser does not support the video tag.
-</video>`;
+    apodMedia.innerHTML = `<iframe
+  width="640"
+  height="360"
+  src=${mediaUrl}
+  frameborder="0"
+  allowfullscreen
+></iframe>`;
   }
-
   if (data.hasOwnProperty("copyright")) {
     copyright.innerHTML = `<p>&copy; ${data.copyright}</p>`;
   }
+}
+
+const randomBtn = document.getElementById("random-btn");
+const randomApod = document.getElementById("random-apod");
+const specificBtn = document.getElementById("specific-btn");
+const specificApod = document.getElementById("specific-apod");
+
+randomBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  let today = new Date();
+  today = dateToISO(today);
+  let firstDate = new Date("June 16, 1995");
+  let randomDate = randomDay(firstDate, today);
+  randomDate = dateToISO(randomDate);
+  let numApods = parseInt(randomApod.value);
+  if (numApods === 1) {
+    additions = `&date=${randomDate}`;
+  } else {
+    let today = new Date();
+    endDate = addDaysToDate(apodStartDate, numApods - 1);
+    if (endDate > today) {
+      endDate = apodStartDate;
+      apodStartDate = addDaysToDate(endDate, 1 - numApods);
+      apodStartDate = dateToISO(apodStartDate);
+    } else {
+      endDate = dateToISO(endDate);
+    }
+    additions = `&start_date=${apodStartDate}&end_date=${endDate}`;
+  }
+  apiQuery = apiUrl + apiKey + additions;
+  fetchAPOD();
+});
+
+specificBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  let numApods = parseInt(specificApod.value);
+  let apodStartDate = startDate.value;
+  let endDate;
+  let additions;
+  if (numApods === 1) {
+    additions = `&date=${apodStartDate}`;
+  } else {
+    let today = new Date();
+    endDate = addDaysToDate(apodStartDate, numApods - 1);
+    if (endDate > today) {
+      endDate = apodStartDate;
+      apodStartDate = addDaysToDate(endDate, 1 - numApods);
+      apodStartDate = dateToISO(apodStartDate);
+    } else {
+      endDate = dateToISO(endDate);
+    }
+    additions = `&start_date=${apodStartDate}&end_date=${endDate}`;
+  }
+  apiQuery = apiUrl + apiKey + additions;
+  fetchAPOD();
+});
+
+function addDaysToDate(date, days) {
+  let newDate = new Date(date);
+  newDate.setDate(date.getDate() + days);
+  return newDate;
+}
+
+function dateToISO(date) {
+  return new Date(date).toISOString().slice(0, 10);
+}
+
+function randomDay(begin, end) {
+  begin = new Date(begin);
+  end = new Date(end);
+  return new Date(
+    begin.getTime() + Math.random() * (end.getTime() - begin.getTime())
+  );
+}
+
+function playVideo(videoUrl) {
+  const iframe = document.createElement("iframe");
+  iframe.src = videoUrl;
+  iframe.width = "100%";
+  iframe.allow = "accelerometer; autoplay; gyroscope; picture-in-picture";
+  iframe.allowFullscreen = true;
+  apodMedia.appendChild(iframe);
 }
 
 fetchAPOD();
